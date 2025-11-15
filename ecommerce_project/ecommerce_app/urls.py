c5605a9637c922aacd326b1c2f9537981aa05eb4
M-Swathi22 <swathi.email1@gmail.com>
2025-11-15 18:14:08 +0530

    Implemented user registration and login functionality with validation

--- ecommerce_project/ecommerce_app/urls.py
+++ ecommerce_project/ecommerce_app/urls.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.urls import path
+from . import views
 
 # Create your views here.
+urlpatterns = [
+     path('register/', views.register, name='register'),
+     path('login/', views.login_view, name='login'),
+]
--- ecommerce_project/ecommerce_app/views.py
+++ ecommerce_project/ecommerce_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Customer
+from .models import Product,Category
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        Customer.objects.create(name=name, email=email, password=password)
+        return redirect('login')
+    return render(request, 'register.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        try:
+            customer = Customer.objects.get(email=email, password=password)
+            request.session['customer_id'] = customer.id
+            return redirect('home')
+        except Customer.DoesNotExist:
+            return render(request, 'login.html', {'error': 'Invalid email or password'})
+    return render(request, 'login.html')
