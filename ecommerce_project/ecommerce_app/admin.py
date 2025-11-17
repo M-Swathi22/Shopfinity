30af13429b77881eec06a674224f904df73017f3
M-Swathi22 <swathi.email1@gmail.com>
2025-11-17 13:34:22 +0530

    Added Cart & Wishlist models + admin registration

--- ecommerce_project/ecommerce_app/admin.py
+++ ecommerce_project/ecommerce_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Product,Customer
+from .models import Category, Product,Customer,Cart,Wishlist
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Customer)
-
+admin.site.register(Cart)
+admin.site.register(Wishlist)
--- ecommerce_project/ecommerce_app/models.py
+++ ecommerce_project/ecommerce_app/models.py
@@ -25,3 +25,21 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer.name} - {self.product.name} - {self.quantity}"
+    def total_price(self):
+        return self.product.price * self.quantity
+
+class Wishlist(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.customer.name} - {self.product.name}"
