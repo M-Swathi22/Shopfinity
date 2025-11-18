7969339b02012f7dbc7fd01688c8c4ab61534b5d
M-Swathi22 <swathi.email1@gmail.com>
2025-11-18 13:21:29 +0530

    Fix static files settings

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'ecommerce_app' / 'static',
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
