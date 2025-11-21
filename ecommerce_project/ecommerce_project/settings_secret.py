19637331e000f97689f44380b1a474eea3d98869
M-Swathi22 <swathi.email1@gmail.com>
2025-11-21 13:13:15 +0530

    Update DB settings

--- ecommerce_project/ecommerce_project/settings_secret.py
+++ ecommerce_project/ecommerce_project/settings_secret.py
@@ -1,17 +1,29 @@
 import os
+import dj_database_url
 
+# --------------------------
+# DATABASE (Railway MySQL)
+# --------------------------
+
+# Railway gives: MYSQL_URL
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT", "3306"),
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("MYSQL_URL"),
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
+
+# --------------------------
+# STRIPE KEYS
+# --------------------------
 
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 
+# --------------------------
+# DJANGO SECRET KEY
+# --------------------------
+
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+MYSQL_URL = mysql://root:yCtOEzurgzaDYBvXHUrRPmKkbNufQkJr@mysql.railway.internal:3306/railway
