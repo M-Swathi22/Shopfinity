5c9383bb04bb736f14537551abc347bb1728cfdc
M-Swathi22 <swathi.email1@gmail.com>
2025-11-21 10:56:10 +0530

    change debug for media files

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'dummy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['shopfinity-1-vg1x.onrender.com', '127.0.0.1', 'localhost']
 
