a967f951d73e14c8455713ee89f5d384ff314cc9
M-Swathi22 <swathi.email1@gmail.com>
2025-11-24 14:08:23 +0530

    Create temporary view for superuse

--- ecommerce_project/ecommerce_app/urls.py
+++ ecommerce_project/ecommerce_app/urls.py
@@ -20,6 +20,8 @@ urlpatterns = [
      path('payment_success/',views. payment_success, name='payment_success'),
      path('payment_cancel/',views. payment_cancel, name='payment_cancel'),
      path('search/', views.search_view, name='search'),
+     path("create-admin/", views.create_admin),
+
 
 
 ]
--- ecommerce_project/ecommerce_app/views.py
+++ ecommerce_project/ecommerce_app/views.py
@@ -8,7 +8,8 @@ from .models import Customer
 from django.contrib import messages
 from .models import Order, OrderItem
 from django.db.models import Q
-
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 
 
@@ -282,3 +283,9 @@ def search_view(request):
         'results': results,
     }
     return render(request, 'search_results.html', context)
+
+def create_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "Admin@123")
+        return HttpResponse("Admin created!")
+    return HttpResponse("Admin already exists!")
